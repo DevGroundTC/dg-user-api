f200abc41ed975ae599054a50ddbd9a2dfdae98c
OlgaZalozhenkova <olga.zalogenkova@mail.ru>
2023-12-08 18:01:16 +0300

    Initial commit

---
+++ src/main/java/com/example/dguserapi/controllers/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.example.dguserapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class HealthCheckController {
+
+    @GetMapping("/healthcheck")
+    public String returnOK() {
+        return "OK";
+    }
+}
---
+++ src/test/java/com/example/dguserapi/DgUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dguserapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgUserApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
